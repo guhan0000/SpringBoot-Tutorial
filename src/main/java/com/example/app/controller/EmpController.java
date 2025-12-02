c09128d2038b70ba89134d2d433a6b4c7b004306
guhan0000 <guhanm0000@gmail.com>
2025-12-02 13:43:51 +0530

    List displayed

--- src/main/java/com/example/app/controller/EmpController.java
+++ src/main/java/com/example/app/controller/EmpController.java
@@ -1,19 +1,25 @@
 package com.example.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-@Controller
+@RestController
+
 public class EmpController {
 	
 	
-	@GetMapping("/")
-	public String getEmpData() {
-		// TODO Auto-generated method stub
-		return "Hello";
+	@GetMapping("/getEmpData")
+	public List<Integer> getEmpData() {
+		List<Integer> list=new ArrayList<>();
+		for(int i=10;i<=100;i+=10) {
+			list.add(i);
+		}
+		return list;
 
 	}
 	@PostMapping("/")
