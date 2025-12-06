fcbc636fdcbc2c18f71c4db67898755449aefbf4
guhan0000 <guhanm0000@gmail.com>
2025-12-06 21:02:22 +0530

    @RequestMaping, @PathVariable, @RequestParam

--- src/main/java/com/example/app/controller/EmpController.java
+++ src/main/java/com/example/app/controller/EmpController.java
@@ -11,16 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.entity.Employee;
 import com.example.app.service.EmpService;
 
 @RestController
+@RequestMapping("/emp")
 
 public class EmpController {
 	@Autowired
 	EmpService service;
+
 	@GetMapping("/getEmpData")
 	private List<Employee> getEmpData() {
 		// TODO Auto-generated method stub
@@ -40,12 +44,28 @@ public class EmpController {
 		service.updateEmpService();
 
 	}
+
 @DeleteMapping("/deleteEmpData/{index}")
 	private String deleteEmpData(@PathVariable int index) {
 		// TODO Auto-generated method stub
 		return service.deleteEmpService(index);
-		
-	
 	}
+
+//Path Variable
+@GetMapping("/getID/{id}")
+	public String getIdString(@PathVariable long id) {
+		return "Id is "+id;
+	}
+
+//Request Parameter
+@GetMapping("/get")
+public String name(@RequestParam String name,@RequestParam String role) {
+	return name +" "+role;
+}
+
+@GetMapping("/salary")
+public double salary(@RequestParam(name = "empSalary") double salary) {
+	return salary;
+}
 
 }
